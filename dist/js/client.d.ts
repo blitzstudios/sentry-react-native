1d9f79fab2fe56526b0f067ae49d10a22308a961
Luke Herbold <luke@sleeper.app>
2022-02-01 17:22:41 -0800

    .gitignore + dist files.

---
+++ dist/js/client.d.ts
@@ -0,0 +1,30 @@
+import { BaseClient } from "@sentry/core";
+import { ReactNativeBackend } from "./backend";
+import { ReactNativeOptions } from "./options";
+/**
+ * The Sentry React Native SDK Client.
+ *
+ * @see ReactNativeOptions for documentation on configuration options.
+ * @see SentryClient for usage documentation.
+ */
+export declare class ReactNativeClient extends BaseClient<ReactNativeBackend, ReactNativeOptions> {
+    /**
+     * Creates a new React Native SDK instance.
+     * @param options Configuration options for this SDK.
+     */
+    constructor(options: ReactNativeOptions);
+    /**
+     * If native client is available it will trigger a native crash.
+     * Use this only for testing purposes.
+     */
+    nativeCrash(): void;
+    /**
+     * Returns whether or not the last run resulted in a crash.
+     */
+    didCrashLastLaunch(): PromiseLike<boolean>;
+    /**
+     * @inheritDoc
+     */
+    close(): PromiseLike<boolean>;
+}
+//# sourceMappingURL=client.d.ts.map
